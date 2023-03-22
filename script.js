a8397bf7642edaca8bda2bb6a7df54ff3a7bd9a0
khayrul200822 <khayrulislam200822@gmail.com>
2023-03-23 00:25:24 +0600

    update js

--- script.js
+++ script.js
@@ -1,26 +1,25 @@
 function sendEmail() {
-    let formInfo = {
+    let params = {
         name: document.getElementById("name").value,
         email: document.getElementById("email").value,
         phone: document.getElementById("phone").value
     }
 
     const serviceID = "service_ncw969m";
-const templateID = "template_6rin00b"
+    const templateID = "template_6rin00b";
 
-emialjs.send(serviceID, templateID, formInfo)
+    emailjs.send(serviceID, templateID, params)
 
-    .then(
-        res => {
-            document.getElementById("name").value = "";
-            document.getElementById("email").value = "";
-            document.getElementById("phone").value = "";
-            console.log(res)
-            alert("Your message sent successfully");
-        }
-    )
+        .then(
+            res => {
+                document.getElementById("name").value = "";
+                document.getElementById("email").value = "";
+                document.getElementById("phone").value = "";
+                console.log(res);
+                prompt("Who are you")
+            }
+        )
 
-    .catch((err) => console.log(err))
+        .catch((err) => console.log(err))
 
 }
-
