7d09f44977d7b34feec84979ffc3de9c444d8e5f
khayrul200822 <khayrulislam200822@gmail.com>
2023-03-23 01:38:44 +0600

    updated

--- script.js
+++ script.js
@@ -6,16 +6,17 @@ function sendEmail() {
 
     const serviceID = "service_ncw969m";
     const templateID = "template_6rin00b";
+    const publickey = "mXR53IPD5No7uGw5U";
 
-    emailjs.send(serviceID, templateID, params)
+    emailjs.send(serviceID, templateID, params,publickey)
 
         .then(
             res => {
                 document.getElementById("name").value = "";
                 document.getElementById("email").value = "";
-                document.getElementById("phone").value = "";
+
                 console.log(res);
-                prompt("Who are you")
+                alert("Who are you")
             }
         )
 
